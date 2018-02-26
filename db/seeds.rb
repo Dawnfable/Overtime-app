2e6bebd70af880fc414d66c07f56d8051b38d913
Dawnfable <DuskDawnfable@gmail.com>
2018-02-26 16:29:33 -0700

    added status to the page

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,10 @@
 
 puts "1 user created"
 
+AdminUser.create(email: "admin@admin.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Admin", last_name: "User")
+
+puts "1 admin user created"
+
 100.times do |post|
   Post.create!(date: Date.today, Rationale: "#{post} rationale content", user_id: @user.id)
 end
