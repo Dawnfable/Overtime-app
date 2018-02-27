df751c9284f86124f3fdbeafd61ff92e55895955
Dawnfable <DuskDawnfable@gmail.com>
2018-02-27 14:09:12 -0700

    added radio button and status check

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,7 +44,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:date, :Rationale)
+    params.require(:post).permit(:date, :Rationale, :status)
   end
 
   def set_post
