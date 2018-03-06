ada596c4e8e6003a96525271b8c3fe0f5d313bd3
Dawnfable <DuskDawnfable@gmail.com>
2018-03-06 16:03:55 -0700

    added navbar element for audit logs

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ AdminUser.create(email: "admin@admin.com",
                  password_confirmation: "asdfasdf", 
                  first_name: "Admin", 
                  last_name: "User",
-                 phone:"13852091055")
+                 phone:"3852091055")
 
 puts "1 admin user created"
 
@@ -23,8 +23,8 @@ end
 puts "100 posts created"
 
 100.times do |audit_log|
-  AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 end
 
-puts "100 audit logs have been created"
+puts "100 audit logs created"
 
