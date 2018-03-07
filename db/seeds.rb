c15094bc2e5c34fd6e4080782d03cba40e3fd745
Dawnfable <DuskDawnfable@gmail.com>
2018-03-07 11:29:22 -0700

    Brought in live data for admin review page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 
 puts "1 user created"
 
-AdminUser.create(email: "admin@admin.com", 
+AdminUser.create(email: "admin@test.com", 
                  password: "asdfasdf", 
                  password_confirmation: "asdfasdf", 
                  first_name: "Admin", 
@@ -17,7 +17,7 @@ AdminUser.create(email: "admin@admin.com",
 puts "1 admin user created"
 
 100.times do |post|
-  Post.create!(date: Date.today, Rationale: "#{post} rationale content", user_id: @user.id, ocertime_request: 1.2)
+  Post.create!(date: Date.today, Rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @user.id, ocertime_request: 2.5)
 end
 
 puts "100 posts created"
