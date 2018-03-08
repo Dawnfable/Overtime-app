2bce1edd9354f2a5a4000c415eda4b84a4d101aa
Dawnfable <DuskDawnfable@gmail.com>
2018-03-08 10:26:24 -0700

    added elements to employee dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :audit_logs, except: [:new, :edit, :destroy]
-
+  resources :audit_logs, except: [:new, :edit, :destory] do
+    member do
+      get :confirm
+    end
+  end
+  
   namespace :admin do
     resources :users
     resources :posts
@@ -10,12 +14,11 @@ Rails.application.routes.draw do
   end
 
   resources :posts do
-  member do
-    get :approve
+    member do
+      get :approve
+    end
   end
-end
 
-  resources :posts
   devise_for :users, skip: [:registrations]
   root to: 'static#homepage'
 end
